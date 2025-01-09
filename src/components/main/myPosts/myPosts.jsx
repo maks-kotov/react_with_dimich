95dc833ae9b4898f7f6e5f0f08e3d982d456c612
maks__kotov <maxsyper8902@gmail.com>
2025-01-09 13:52:32 +0500

    теперь данные для переписки берутся из массива

--- src/components/main/myPosts/myPosts.jsx
+++ src/components/main/myPosts/myPosts.jsx
@@ -1,8 +1,13 @@
 import classes from "./myPosts.module.css";
 import Post from "./post/post";
 const MyPosts = () => {
+  const postData = [
+    {myText: 'today is 05.01.2025', countLikes: '1', id: 0},
+    {myText: 'i cheepful now', countLikes: '2', id: 1},
+    {myText: 'i am coder', countLikes: '999', id: 2}
+  ]
   return (
-    <div>
+    <div className={classes.myPostsContainer}>
       <div className={classes.myPosts}>My posts</div>
       <form className={classes.form}>
         <textarea
@@ -11,9 +16,10 @@ const MyPosts = () => {
         ></textarea>
         <button className={classes.button}>Send</button>
       </form>
-      <Post myText='today is 05.01.2025' countLikes='1'></Post>
-      <Post myText='i cheepful now' countLikes='2'></Post>
-      <Post myText='i am coder' countLikes='999'></Post>
+      <Post myText={postData[0].myText} countLikes={postData[0].countLikes}></Post>
+      <Post myText={postData[1].myText} countLikes={postData[1].countLikes}></Post>
+      <Post myText={postData[2].myText} countLikes={postData[2].countLikes}></Post>
+
     </div>
   );
 };
